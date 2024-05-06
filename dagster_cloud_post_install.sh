c9689a8653dd4ebfd6b1f710f5796bc578ce6d74
Luke Salomone <ldsalomone@gmail.com>
2024-05-06 11:30:37 -0600

    Update dagster_cloud_post_install.sh

--- dagster_cloud_post_install.sh
+++ dagster_cloud_post_install.sh
@@ -5,6 +5,6 @@ apt-get install -y google-chrome-stable
 
 # install chromedriver
 apt-get install -yqq unzip
-wget -O /tmp/chromedriver.zip https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/`curl -sS https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE`/linux64/chromedriver-linux64.zip
+wget -O /tmp/chromedriver.zip https://storage.googleapis.com/chrome-for-testing-public/`curl -sS https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_STABLE`/linux64/chromedriver-linux64.zip
 unzip /tmp/chromedriver.zip chromedriver-linux64/chromedriver -d /tmp
 cp /tmp/chromedriver-linux64/chromedriver /usr/local/bin/
