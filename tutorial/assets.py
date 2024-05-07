aae065195712605fb754f46b5fe3ee4a7bcc346e
Luke Salomone <ldsalomone@gmail.com>
2024-05-06 18:45:33 -0600

    Update assets.py

--- tutorial/assets.py
+++ tutorial/assets.py
@@ -8,7 +8,7 @@ import base64
 from io import BytesIO
 
 import matplotlib.pyplot as plt
-from dagster import AssetExecutionContext, MetadataValue, asset, MaterializeResult
+from dagster import AssetExecutionContext, MetadataValue, asset, MaterializeResult, get_dagster_logger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -18,16 +18,17 @@ from webdriver_manager.core.os_manager import ChromeType
 
 
 def get_table_data(driver):
+    my_logger = get_dagster_logger()
     # Find the table element by its ID
     table = driver.find_element(By.XPATH, '//*[@id="ContentPlaceHolder2_LData"]')
     # Find all rows of the table
     rows = table.find_elements(By.TAG_NAME, "tr")
 
     table_data = []
-    print(len(rows))
+    my_logger.info(len(rows))
     for i, row in enumerate(rows[4:]):  # Skip the first four rows
         if i % 100 == 0:
-            print(f"Processing row {i}")
+            my_logger.info(f"Processing row {i}")
         # Find all cells of the row
         cells = row.find_elements(By.TAG_NAME, "td")
         # Extract text from each cell and append to table_data
@@ -135,6 +136,7 @@ def most_frequent_words() -> MaterializeResult:
 
 @asset
 def run_selenium() -> None:
+    my_logger = get_dagster_logger()
     # import chromedriver_autoinstaller
 
     # chromedriver_autoinstaller.install()
@@ -164,7 +166,7 @@ def run_selenium() -> None:
     all_data = []
 
     for letter in "ABCDEGFHIJKLMNOPQRSTUVWXYZ":
-        print(f"Scraping for {letter}...")
+        my_logger.info(f"Scraping for {letter}...")
         last_name_box = driver.find_element(
             By.XPATH, '//*[@id="ContentPlaceHolder2_TLname"]'
         )
@@ -176,9 +178,9 @@ def run_selenium() -> None:
         )
         search_button.click()
         time.sleep(5)
-        print("---getting table data...")
+        my_logger.info("---getting table data...")
         t_data = get_table_data(driver)
-        print(f"---{len(t_data)} rows retrieved...")
+        my_logger.info(f"---{len(t_data)} rows retrieved...")
         all_data.extend(t_data)
 
     print(all_data)
